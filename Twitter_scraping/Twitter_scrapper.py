8fa240c0fb5e0cd2e945c8e4ad10eafc6992639f
Aaditya Narain <aaditya.narain.avna@gmail.com>
2023-10-29 01:17:52 +0530

    wow

--- Twitter_scraping/Twitter_scrapper.py
+++ Twitter_scraping/Twitter_scrapper.py
@@ -17,7 +17,7 @@ time.sleep(2)
 driver.execute_script("window.scrollTo(0, 2000);")
 page_source = driver.page_source
 soup = BS(page_source, features='html.parser')
-with open('text.html', 'a') as f:
+with open('text.html', 'w') as f:
     f.write(str(soup))
     driver.execute_script("window.scrollTo(0, 2000);")
 page_source = driver.page_source
