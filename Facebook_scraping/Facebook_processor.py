f0e1c1a3c7c358df0a32d53bc36e0c6ba341d424
Aaditya Narain <aaditya.narain.avna@gmail.com>
2023-10-29 11:39:39 +0530

    Facebook done

---
+++ Facebook_scraping/Facebook_processor.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup as BS
+import time
+
+page_source = ''
+with open('post.html', 'r') as f:
+    page_source = f.read()
+soup = BS(page_source, features='html.parser')
+
+texts = soup.find_all(
+    'div', class_='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs x126k92a')
+extracted_data = ""
+for i in texts:
+    extracted_data += i.text +"\n"
+
+with open('scraed_data.txt', 'w') as f:
+    f.write(extracted_data)
+    f.write('\n')
